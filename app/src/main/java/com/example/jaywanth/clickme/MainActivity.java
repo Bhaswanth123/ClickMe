af9afafea913b701db68d1089a9f140d333216d9
Bhaswanth <classybasu@gmail.com>
2018-08-05 12:11:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jaywanth/clickme/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.jaywanth.clickme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickMe(View view)
+    {
+        Log.i("Info","Hi I have successfully created a button!Hurrah!");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
